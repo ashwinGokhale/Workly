fbaab0d9ce964b31b2c9efa8559bc3af2b36b8bf
Ashwin Gokhale <ashwin.gokhale98@gmail.com>
2018-11-29 21:07:29 -0500

    Finished Table

--- frontend/src/components/Common/SearchColumn.js
+++ frontend/src/components/Common/SearchColumn.js
@@ -1,126 +1,59 @@
 import React from 'react';
 import { Table, Input, Button, Icon } from 'antd';
 
-export default class SearchColumn extends Table.Column {
-	constructor(props) {
-		super(props);
-		console.log('Search Column made:', this.props);
-	}
+// export default class SearchColumn extends Table.Column {
+// 	constructor(props) {
+// 		super(props);
+// 		console.log('Search Column made:', this.props);
+// 	}
 
-	state = {
-		searchText: ''
-	};
+// 	state = {
+// 		searchText: ''
+// 	};
 
-	handleSearch = (selectedKeys, confirm) => () => {
-		confirm();
-		this.setState({ searchText: selectedKeys[0] });
-	};
+// 	handleSearch = (selectedKeys, confirm) => () => {
+// 		confirm();
+// 		this.setState({ searchText: selectedKeys[0] });
+// 	};
 
-	handleReset = clearFilters => () => {
-		clearFilters();
-		this.setState({ searchText: '' });
-	};
+// 	handleReset = clearFilters => () => {
+// 		clearFilters();
+// 		this.setState({ searchText: '' });
+// 	};
 
-	filterDropdown = ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
-		<div className="custom-filter-dropdown">
-			<Input
-				ref={ele => (this.searchInput = ele)}
-				placeholder={`Search ${this.props.title}`}
-				value={selectedKeys[0]}
-				onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
-				onPressEnter={this.handleSearch(selectedKeys, confirm)}
-			/>
-			<Button type="primary" onClick={this.handleSearch(selectedKeys, confirm)}>
-				Search
-			</Button>
-			<Button onClick={this.handleReset(clearFilters)}>Reset</Button>
-		</div>
-	);
-
-	filterIcon = filtered => (
-		<Icon type="filter" style={{ color: filtered ? '#108ee9' : '#aaa' }} />
-	);
-	onFilter = (value, record) =>
-		record[this.props.key] &&
-		record[this.props.key].toLowerCase().includes(value.toLowerCase());
-
-	onFilterDropdownVisibleChange = visible => {
-		if (visible) {
-			setTimeout(() => {
-				this.searchInput.focus();
-			});
-		}
-	};
-	renderColumn = text => {
-		console.log('Rendering column:', text);
-		const { searchText } = this.state;
-		return searchText ? (
-			<span>
-				{text.split(new RegExp(`(?<=${searchText})|(?=${searchText})`, 'i')).map(
-					(fragment, i) =>
-						fragment.toLowerCase() === searchText.toLowerCase() ? (
-							<span key={i} className="highlight">
-								{fragment}
-							</span>
-						) : (
-							fragment
-						) // eslint-disable-line
-				)}
-			</span>
-		) : (
-			text
-		);
-	};
-
-	render() {
-		const { title, key } = this.props;
-		console.log('Search Column Props', this.props);
-		return (
-			<Table.Column
-				title={title}
-				key={key}
-				// dataIndex={key}
-				filterDropdown={this.filterDropdown}
-				filterIcon={this.filterIcon}
-				onFilter={this.onFilter}
-				onFilterDropdownVisibleChange={this.onFilterDropdownVisibleChange}
-				render={this.renderColumn}
-			/>
-		);
-	}
-}
-
-// export default (title, key, handleSearch, handleReset, searchText) => ({
-// 	title,
-// 	dataIndex: key,
-// 	key,
-// 	filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+// 	filterDropdown = ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
 // 		<div className="custom-filter-dropdown">
 // 			<Input
-// 				// ref={ele => (this.searchInput = ele)}
-// 				placeholder={`Search ${title}`}
+// 				ref={ele => (this.searchInput = ele)}
+// 				placeholder={`Search ${this.props.title}`}
 // 				value={selectedKeys[0]}
 // 				onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
-// 				onPressEnter={handleSearch(selectedKeys, confirm)}
+// 				onPressEnter={this.handleSearch(selectedKeys, confirm)}
 // 			/>
-// 			<Button type="primary" onClick={handleSearch(selectedKeys, confirm)}>
+// 			<Button type="primary" onClick={this.handleSearch(selectedKeys, confirm)}>
 // 				Search
 // 			</Button>
-// 			<Button onClick={handleReset(clearFilters)}>Reset</Button>
+// 			<Button onClick={this.handleReset(clearFilters)}>Reset</Button>
 // 		</div>
-// 	),
-// 	filterIcon: filtered => <Icon type="filter" style={{ color: filtered ? '#108ee9' : '#aaa' }} />,
-// 	onFilter: (value, record) =>
-// 		record[key] && record[key].toLowerCase().includes(value.toLowerCase()),
-// 	onFilterDropdownVisibleChange: visible => {
+// 	);
+
+// 	filterIcon = filtered => (
+// 		<Icon type="filter" style={{ color: filtered ? '#108ee9' : '#aaa' }} />
+// 	);
+// 	onFilter = (value, record) =>
+// 		record[this.props.key] &&
+// 		record[this.props.key].toLowerCase().includes(value.toLowerCase());
+
+// 	onFilterDropdownVisibleChange = visible => {
 // 		if (visible) {
 // 			setTimeout(() => {
-// 				// this.searchInput.focus();
+// 				this.searchInput.focus();
 // 			});
 // 		}
-// 	},
-// 	render: text => {
-// 		// const { searchText } = this.state;
+// 	};
+// 	renderColumn = text => {
+// 		console.log('Rendering column:', text);
+// 		const { searchText } = this.state;
 // 		return searchText ? (
 // 			<span>
 // 				{text.split(new RegExp(`(?<=${searchText})|(?=${searchText})`, 'i')).map(
@@ -137,5 +70,63 @@ export default class SearchColumn extends Table.Column {
 // 		) : (
 // 			text
 // 		);
+// 	};
+
+// 	render() {
+// 		const { title, key } = this.props;
+// 		console.log('Search Column Props', this.props);
+// 		return (
+// 			<Table.Column
+// 				title={title}
+// 				key={key}
+// 				// dataIndex={key}
+// 				filterDropdown={this.filterDropdown}
+// 				filterIcon={this.filterIcon}
+// 				onFilter={this.onFilter}
+// 				onFilterDropdownVisibleChange={this.onFilterDropdownVisibleChange}
+// 				render={this.renderColumn}
+// 			/>
+// 		);
 // 	}
-// });
+// }
+
+export default (title, key, handleSearch, handleReset, searchText) => ({
+	title,
+	dataIndex: key,
+	key,
+	filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+		<div className="custom-filter-dropdown">
+			<Input
+				placeholder={`Search ${title}`}
+				value={selectedKeys[0]}
+				onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+				onPressEnter={handleSearch(selectedKeys, confirm, `${key}Text`)}
+			/>
+			<Button type="primary" onClick={handleSearch(selectedKeys, confirm, `${key}Text`)}>
+				Search
+			</Button>
+			<Button onClick={handleReset(clearFilters, `${key}Text`)}>Reset</Button>
+		</div>
+	),
+	filterIcon: filtered => <Icon type="filter" style={{ color: filtered ? '#108ee9' : '#aaa' }} />,
+	onFilter: (value, record) =>
+		record[key] && record[key].toLowerCase().includes(value.toLowerCase()),
+	render: text => {
+		return searchText ? (
+			<span>
+				{text.split(new RegExp(`(?<=${searchText})|(?=${searchText})`, 'i')).map(
+					(fragment, i) =>
+						fragment.toLowerCase() === searchText.toLowerCase() ? (
+							<span key={i} className="highlight">
+								{fragment}
+							</span>
+						) : (
+							fragment
+						) // eslint-disable-line
+				)}
+			</span>
+		) : (
+			text
+		);
+	}
+});
--- frontend/src/components/Home/JobTable.js
+++ frontend/src/components/Home/JobTable.js
@@ -1,159 +1,64 @@
 import React from 'react';
-import { Table, Input, Button, Icon } from 'antd';
+import { Table } from 'antd';
 import { statuses } from '../../constants';
 import SearchColumn from '../Common/SearchColumn';
 import './JobTable.css';
 
-const data = [
-	{
-		key: '1',
-		company: 'John Brown',
-		age: 32,
-		address: 'New York No. 1 Lake Park'
-	},
-	{
-		key: '2',
-		company: 'Brown Hames',
-		age: 23,
-		address: 'London, Somrthng'
-	}
-];
-
 class JobTable extends React.Component {
 	state = {
-		searchText: ''
+		companyText: '',
+		roleText: '',
+		emailText: '',
+		locationText: '',
+		recruiterText: ''
 	};
 
-	handleSearch = (selectedKeys, confirm) => () => {
+	handleSearch = (selectedKeys, confirm, text) => () => {
 		confirm();
-		this.setState({ searchText: selectedKeys[0] });
+		this.setState({ [text]: selectedKeys[0] });
 	};
 
-	handleReset = clearFilters => () => {
+	handleReset = (clearFilters, text) => () => {
 		clearFilters();
-		this.setState({ searchText: '' });
+		this.setState({ [text]: '' });
 	};
 
 	render() {
-		console.log('Job Table props:', this.props);
 		const { jobs, loading } = this.props;
-		const data = jobs.map(job => ({
-			key: job._id,
-			...job,
-			coverLetter: job.coverLetter ? 'Yes' : 'No'
-		}));
+		const data = jobs.map(job => {
+			job = {
+				key: job._id,
+				...job,
+				coverLetter: job.coverLetter ? 'Yes' : 'No'
+			};
+			Object.keys(job).forEach(key => {
+				if (!job[key]) job[key] = 'None';
+			});
+			return job;
+		});
+
 		const columns = [
-			{
-				title: 'Company',
-				dataIndex: 'company',
-				key: 'company',
-				filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
-					<div className="custom-filter-dropdown">
-						<Input
-							ref={ele => (this.searchInput = ele)}
-							placeholder="Search Company"
-							value={selectedKeys[0]}
-							onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
-							onPressEnter={this.handleSearch(selectedKeys, confirm)}
-						/>
-						<Button type="primary" onClick={this.handleSearch(selectedKeys, confirm)}>
-							Search
-						</Button>
-						<Button onClick={this.handleReset(clearFilters)}>Reset</Button>
-					</div>
-				),
-				filterIcon: filtered => (
-					<Icon type="filter" style={{ color: filtered ? '#108ee9' : '#aaa' }} />
-				),
-				onFilter: (value, record) =>
-					record.company.toLowerCase().includes(value.toLowerCase()),
-				onFilterDropdownVisibleChange: visible => {
-					if (visible) {
-						setTimeout(() => {
-							this.searchInput.focus();
-						});
-					}
-				},
-				render: text => {
-					const { searchText } = this.state;
-					return searchText ? (
-						<span>
-							{text
-								.split(new RegExp(`(?<=${searchText})|(?=${searchText})`, 'i'))
-								.map(
-									(fragment, i) =>
-										fragment.toLowerCase() === searchText.toLowerCase() ? (
-											<span key={i} className="highlight">
-												{fragment}
-											</span>
-										) : (
-											fragment
-										) // eslint-disable-line
-								)}
-						</span>
-					) : (
-						text
-					);
-				}
-			},
-			{
-				title: 'Role',
-				dataIndex: 'role',
-				key: 'role',
-				filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
-					<div className="custom-filter-dropdown">
-						<Input
-							ref={ele => (this.searchInput = ele)}
-							placeholder="Search Role"
-							value={selectedKeys[0]}
-							onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
-							onPressEnter={this.handleSearch(selectedKeys, confirm)}
-						/>
-						<Button type="primary" onClick={this.handleSearch(selectedKeys, confirm)}>
-							Search
-						</Button>
-						<Button onClick={this.handleReset(clearFilters)}>Reset</Button>
-					</div>
-				),
-				filterIcon: filtered => (
-					<Icon type="filter" style={{ color: filtered ? '#108ee9' : '#aaa' }} />
-				),
-				onFilter: (value, record) =>
-					record.company.toLowerCase().includes(value.toLowerCase()),
-				onFilterDropdownVisibleChange: visible => {
-					if (visible) {
-						setTimeout(() => {
-							this.searchInput.focus();
-						});
-					}
-				},
-				render: text => {
-					const { searchText } = this.state;
-					return searchText ? (
-						<span>
-							{text
-								.split(new RegExp(`(?<=${searchText})|(?=${searchText})`, 'i'))
-								.map(
-									(fragment, i) =>
-										fragment.toLowerCase() === searchText.toLowerCase() ? (
-											<span key={i} className="highlight">
-												{fragment}
-											</span>
-										) : (
-											fragment
-										) // eslint-disable-line
-								)}
-						</span>
-					) : (
-						text
-					);
-				}
-			},
-			{
-				title: 'Email',
-				dataIndex: 'email',
-				key: 'email'
-			},
+			SearchColumn.bind(this)(
+				'Company',
+				'company',
+				this.handleSearch,
+				this.handleReset,
+				this.state.companyText
+			),
+			SearchColumn.bind(this)(
+				'Role',
+				'role',
+				this.handleSearch,
+				this.handleReset,
+				this.state.roleText
+			),
+			SearchColumn.bind(this)(
+				'Email',
+				'email',
+				this.handleSearch,
+				this.handleReset,
+				this.state.emailText
+			),
 			{
 				title: 'Status',
 				dataIndex: 'status',
@@ -167,18 +72,34 @@ class JobTable extends React.Component {
 			{
 				title: 'Cover Letter',
 				dataIndex: 'coverLetter',
-				key: 'coverLetter'
+				key: 'coverLetter',
+				filters: [
+					{
+						text: 'Yes',
+						value: 'Yes'
+					},
+					{
+						text: 'No',
+						value: 'No'
+					}
+				],
+				onFilter: (value, record) =>
+					record.coverLetter && record.coverLetter.indexOf(value) === 0
 			},
-			{
-				title: 'Location',
-				dataIndex: 'location',
-				key: 'location'
-			},
-			{
-				title: 'Recruiter',
-				dataIndex: 'recruiter',
-				key: 'recruiter'
-			}
+			SearchColumn.bind(this)(
+				'Location',
+				'location',
+				this.handleSearch,
+				this.handleReset,
+				this.state.locationText
+			),
+			SearchColumn.bind(this)(
+				'Recruiter',
+				'recruiter',
+				this.handleSearch,
+				this.handleReset,
+				this.state.recruiterText
+			)
 			// {
 			// 	company: '', // required
 			// 	role: '', // required
